f3cb86b7422b684c8b65121a33d101779bcf7f2f
ABdulaziz <a.captain.b@gmail.com>
2024-08-25 21:33:27 -0600

    controller

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,63 +1,73 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 
-// Define reactive variables for speed, time, and distance
-const speed = ref(null);
-const time = ref(null);
-const distance = ref(null);
+// Initialize variables with default values
+const selectedCalculation = ref('');
+const speed = ref(0);
+const time = ref(0);
+const distance = ref(0);
 
-const calculateDistance = () => {
-  if (speed.value !== null && time.value !== null) {
+// Calculation function with robust checks and initializations
+const calculate = () => {
+  console.log('Calculating:', selectedCalculation.value);
+  if (selectedCalculation.value === 'speed' && time.value !== 0 && distance.value !== 0) {
+    speed.value = distance.value / time.value;
+  } else if (selectedCalculation.value === 'time' && speed.value !== 0 && distance.value !== 0) {
+    time.value = distance.value / speed.value;
+  } else if (selectedCalculation.value === 'distance' && speed.value !== 0 && time.value !== 0) {
     distance.value = speed.value * time.value;
   }
 };
-
-const calculateTime = () => {
-  if (speed.value !== null && distance.value !== null) {
-    time.value = distance.value / speed.value;
-  }
-};
-
-const calculateSpeed = () => {
-  if (time.value !== null && distance.value !== null) {
-    speed.value = distance.value / time.value;
-  }
-};
-
-// Watchers to trigger calculation based on input changes
-watch(speed, (newSpeed) => {
-  if (newSpeed !== null) {
-    calculateDistance();
-    calculateTime();
-  }
-});
-
-watch(time, (newTime) => {
-  if (newTime !== null) {
-    calculateDistance();
-    calculateSpeed();
-  }
-});
-
-watch(distance, (newDistance) => {
-  if (newDistance !== null) {
-    calculateSpeed();
-    calculateTime();
-  }
-});
 </script>
 
 <template>
   <div>
-    <input type="number" v-model="speed" placeholder="Enter speed">
-    <input type="number" v-model="time" placeholder="Enter time">
-    <input type="number" v-model="distance" placeholder="Enter distance">
-    <p>Speed: {{ speed }}</p>
-    <p>Time: {{ time }}</p>
-    <p>Distance: {{ distance }}</p>
+    <h2>Select a Calculation:</h2>
+    <button @click="selectedCalculation = 'speed'">Calculate Speed</button>
+    <button @click="selectedCalculation = 'time'">Calculate Time</button>
+    <button @click="selectedCalculation = 'distance'">Calculate Distance</button>
+
+    <div v-if="selectedCalculation">
+      <h3>Enter Values:</h3>
+
+      <div v-if="selectedCalculation === 'speed'">
+        <input required min="1" type="number" v-model.number="distance" placeholder="Enter distance" />
+        <input required min="1" type="number" v-model.number="time" placeholder="Enter time" />
+      </div>
+
+      <div v-if="selectedCalculation === 'time'">
+        <input required min="1" type="number" v-model.number="speed" placeholder="Enter speed" />
+        <input required min="1" type="number" v-model.number="distance" placeholder="Enter distance" />
+      </div>
+
+      <div v-if="selectedCalculation === 'distance'">
+        <input required min="1" type="number" v-model.number="speed" placeholder="Enter speed" />
+        <input required min="1" type="number" v-model.number="time" placeholder="Enter time" />
+      </div>
+
+      <button @click="calculate">Calculate</button>
+
+      <!-- Conditional Rendering of Results -->
+      <div v-if="selectedCalculation === 'speed' && speed !== 0">
+        <p>Calculated Speed: {{ speed }} m/p</p>
+      </div>
+      <div v-if="selectedCalculation === 'time' && time !== 0">
+        <p>Calculated Time: {{ time }} hours</p>
+      </div>
+      <div v-if="selectedCalculation === 'distance' && distance !== 0">
+        <p>Calculated Distance: {{ distance }} Miles</p>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss">
-/* Add your styles here */
+<style scoped>
+button {
+  margin: 0.5em;
+}
+
+input {
+  display: block;
+  margin: 1em 0;
+}
 </style>
